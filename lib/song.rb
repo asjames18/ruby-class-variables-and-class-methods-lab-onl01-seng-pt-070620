8044bd408f06b19b4139c1264a4e0426caa1300c
Antonio James <asjames18@gmail.com>
2020-07-23 21:26:16 +0000

    Automatically backed up by Learn

---
+++ lib/song.rb
@@ -0,0 +1,62 @@
+class Song
+
+  @@count = 0
+  @@artists = []
+  @@genres = []
+
+
+
+  def initialize(name, artist, genre)
+    @name = name
+    @artist = artist
+    @genre = genre
+    @@count += 1
+    @@artists << artist
+    @@genres << genre
+
+
+end
+
+
+  def name=(name)
+    @name = names
+  end
+  def name
+    @name
+  end
+
+  def artist=(artist)
+    @artist = artist
+
+  end
+
+  def artist
+    @artist
+  end
+
+  def genre=(genre)
+    @genre = genre
+  end
+
+  def genre
+    @genre
+  end
+
+  def self.count
+    @@count
+  end
+
+  def self.artists
+    @@artists.uniq
+  end
+  def self.genres
+    @@genres.uniq
+  end
+
+  def self.genre_count
+    music = {}
+    @@genres.each do |genre|
+
+    end
+  end
+end
